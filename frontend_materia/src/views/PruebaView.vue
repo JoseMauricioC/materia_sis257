39f4bd25d2f89b256d48283294a21fd9de5569e1
JoseMauricioC <josemauriciocamacho1@gmail.com>
2024-10-27 21:23:30 -0300

    primero_vue

---
+++ frontend_materia/src/views/PruebaView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const titulo = ref<string>('Prueba 2024')
+const contador = ref<number>(0)
+
+function incrementar() {
+  contador.value++
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ titulo }} - {{ contador }}</h1>
+    Cambiar Titulo: <input type="text" v-model="titulo" /><br />
+    <button v-on:click="incrementar">Incrementar</button>
+
+    <p v-if="contador % 2 == 0">Es par</p>
+    <p v-else>Es impar</p>
+  </div>
+</template>
+
+<style></style>
